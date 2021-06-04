b8f5eb552954b298f6ef91db9c59ac50e0569efa
walter-cc <imwalter8@gmail.com>
2021-06-04 16:44:38 +0800

    Setep of starting python

---
+++ IDE_Python_Setup.c
@@ -0,0 +1,28 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 06:25:39 2020
+
+@author: cc
+
+# 此檔案為ANSI檔案
+
+anaconda→ spyder → 啟動python
+
+## 設定介面 :
+
+### 整體字型大小 :
+
+tools > preferences > general > appearance > fonts
+
+### 介面顏色 :
+
+tools > preferences > general > appearance > syntax coloring
+
+### 介面開關 :
+
+views > panes
+
+"""
+
+
